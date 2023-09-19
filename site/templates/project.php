77d1dde6bac9f55a86fec7b9b142aff029162969
kareldecoene <info.presentonline@gmail.com>
2023-09-19 14:09:02 +0200

    working on related project data

--- site/templates/project.php
+++ site/templates/project.php
@@ -14,7 +14,16 @@ $data = [
     "contentTitle" => $page->contentTitle()->value(),
     "contentText" => $page->contentText()->toBlocks()->toArray(),
     "contentImage" => $page->contentImage()->toFile()->toArray(),
-    "relatedProjects" => $page->relatedProjects()->toPages()->toArray(),
+    "relatedProjects" => $page->relatedProjects()->toPages()->map(fn ($project) => [
+        "id" => $project->id(),
+        "title" => $project->title()->value(),
+        "slug" => $project->slug(),
+        "cover" => $project->carouselImages()->first()?->toFile()->toArray(),
+        "isprojectnew" => $project->isProjectNew()->toBoolean()->value(),
+        "longtitle" => $project->longTitle()->value(),
+        "descriptioncard" => $page->descriptionCard()->value(),
+        "tags" => $page->tags()->value(),
+    ]),
 ];
 
 echo \Kirby\Data\Json::encode($data);
